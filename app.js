fa549e0ac907778732bf2c7a91a68dc1ebae2d8e
Muzuri Aimable <aimable.muzuri@ircad.africa>
2022-07-26 15:53:20 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,79 @@
+const middleWare = require('./logger')
+const Joi = require('joi');
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const app = express();
+app.use(express.json())
+const courses =[
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+]
+
+app.use((req, res, next)=>{
+    console.log(`This is a middle ware for each request ${req}`)
+    next()
+})
+const env = process.env.NODE_ENV // undefined
+// console.log(`Node Environment is ${app.get('env')}`)
+// console.log(`Node env ${env}`)
+if(app.get('env')=== 'production'){
+    app.use(morgan('tiny'));
+    console.log('enabled morgan')
+}
+
+
+app.use(middleWare);
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(helmet());
+app.use(morgan('tiny'));
+
+app.get('/', function (req, res) {
+  res.send('Hello world')
+})
+app.get('/api/courses', function (req, res) {
+    res.send(courses)
+  })
+
+app.put('/api/courses/:id', (req, res)=> {
+    const id = req.params.id;
+    courses.find(a => a.id = id).name = req.body.name;
+  res.send(courses);
+})
+
+app.put('/api/courses/:id', (req, res)=> {
+    const id = req.params.id;
+    courses.find(a => a.id = id).name = req.body.name;
+  res.send(courses);
+})
+
+app.post('/api/courses', (req, res)=> {
+ const schema = {
+    name: Joi.string().min(3).required()
+ }
+ const result = Joi.validate(req.body, schema);
+ 
+    if(result.error)
+    {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course)
+})
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`you are listening to port ${port}`))
+
+function validate(request){
+
+
+}
